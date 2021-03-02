0dd4e819f9676318602e55ab8b03d06c213919d2
wilson0922 <tree70373@gmail.com>
2021-03-02 18:15:31 +0800

    basic version

---
+++ guess_num.py
@@ -0,0 +1,14 @@
+import random
+r = random.randint(1,100)
+while True:
+	num = input('請猜一個1~100的數字:')
+	num = int(guess)
+	if num == r:
+		print('終於猜對了')
+		break
+	elif num > r:
+		print('你猜的比答案大')
+		continue
+	elif num < r:
+		print('你猜的比答案小')
+		continue
